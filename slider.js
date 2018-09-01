3152f50984a6c6ebbf85163bdeb5cf5bd3dbdd53
hamza <med-hamza911@hotmail.com>
2018-09-01 14:23:10 +0100

    improve code quality add autoplay

--- slider.js
+++ slider.js
@@ -1,27 +1,48 @@
-function removeActiveSlide() {
+function addLoopBack(all) {
+    if (isActive(all.item(all.length - 1)))
+        all.item(0).className = "slide loopback";
+    else if (!isActive(all.item(0)))
+        all.item(0).className = "slide";
+}
+
+function isActive(ele) {
+    return ele.className.indexOf("active") !== -1;
+}
+
+function nextElement(back) {
+    back = back === undefined ? false : back;
+    var all = document.querySelectorAll(".slide"), next,i;
+    for (i = 0; i < all.length; i++)
+        if (back) {// default is false
+            if (i < (all.length - 1) && isActive(all.item(i + 1)))
+                next = all.item(i);
+            if (isActive(all.item(0)))
+                next = all.item(all.length - 1);
+        } else {//next
+            if (i > 0 && isActive(all.item(i - 1)))
+                next = all.item(i);
+            if (isActive(all.item(all.length - 1)))
+                next = all.item(0);
+        }
     document.querySelector(".slide.active").className = "slide";
+    next.className = "slide active";
+    addLoopBack(all);
+    autoplay();
 }
-function addLoopBack(){
-    if(document.querySelector(".slider .slide:last-child").className.indexOf("active")!==-1)
-        document.querySelectorAll(".slide").item(0).className = "slide loopback";
-    else if (document.querySelectorAll(".slide").item(0).className.indexOf("active")===-1)
-        document.querySelectorAll(".slide").item(0).className = "slide";
+
+function autoplay(){
+    if(typeof autoplayTimeout !== "undefined")
+    clearTimeout(autoplayTimeout);
+    autoplayTimeout=setTimeout(function (){
+        nextElement();
+        autoplay();
+    },5000);
 }
+autoplay();
+
 document.querySelector(".slider .arrowLeft").onclick = function () {
-    var prev = document.querySelector(".slide.active").previousElementSibling;
-    removeActiveSlide();
-    if (prev.className.indexOf("slide") !== -1)
-        prev.className = "slide active";
-    else
-        document.querySelector(".slider .slide:last-child").className = "slide active";
-    addLoopBack();
+    nextElement(true);
 };
 document.querySelector(".slider .arrowRight").onclick = function () {
-    var next = document.querySelector(".slide.active").nextElementSibling;
-    removeActiveSlide();
-    if (next !== null)
-        next.className = "slide active";
-    else
-        document.querySelectorAll(".slide").item(0).className = "slide active";
-    addLoopBack();
+    nextElement();
 };
